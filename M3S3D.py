ec4d6832b75ffe42c130cd0e614e7370522fccf1
FranFuentes <francisca.fuentesa12@gmail.com>
2023-05-18 21:58:03 -0400

    carga de trabajo m3s3d

---
+++ M3S3D.py
@@ -0,0 +1,7 @@
+# Nombraremos las variables y les asignaremos su valor a cada una
+a = 20
+b = 30
+# asignaremos una nueva variable con la operacion aritmetica solicitada
+multiplicacion = a*b
+# imprimiremos el valor de la variable
+print(multiplicacion)
